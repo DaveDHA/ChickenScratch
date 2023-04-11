b9555c3a9040614fb874475765754b54f8159e57
David Andrews <dave@sanitychallenged.com>
2023-04-11 18:54:07 -0400

    Formatters working

---
+++ src/ChickenScratch.Interactive/Extension.fs
@@ -0,0 +1,27 @@
+﻿module ChickenScratch.Interactive.Extension
+
+open ChickenScratch
+open Microsoft.DotNet.Interactive.Formatting
+
+
+let private registerScratchNodeFormatter() =
+    Formatter.Register((fun (n : ScratchNode) -> n.AsHtmlString), mimeType = "text/html")
+
+
+let private registerScratchElementFormatter() =
+    Formatter.Register((fun (e : ScratchElement) -> e.AsHtmlString), mimeType = "text/html")
+
+
+let private registerScratchNodeSourceFormatter() = 
+        let formatScratchNodeSource (source : IScratchNodeSource) = 
+            source.GetScratchNodes() 
+            |> List.map(fun node -> node.AsHtmlString) 
+            |> String.concat "\n"
+
+        Formatter.Register<IScratchNodeSource>(formatScratchNodeSource, mimeType = "text/html")
+
+
+let Load() = 
+    registerScratchNodeFormatter()
+    registerScratchElementFormatter()
+    registerScratchNodeSourceFormatter()    
