76d0e54ab76589a2b8b1c772c747809c467c4d83
David Andrews <dave@sanitychallenged.com>
2023-04-12 11:12:52 -0400

    Updated build and publish script

--- Build-Test-Publish.ps1
+++ Build-Test-Publish.ps1
@@ -1,18 +1,48 @@
-$apiKey = Read-Host -Prompt "Enter the Nuget API key"
+function Write-Status($message, $status) {
+    switch ($status) {
+        "Success" { $color = "Green" }
+        "Failure" { $color = "Red" }                   
+        Default { $color = "Cyan" }
+    }
 
-Remove-item ./publish -Recurse 
+    Write-Host "`n---------------------------------------------------------------------------" -ForegroundColor $color
+    Write-Host $message -ForegroundColor $color
+    Write-Host "---------------------------------------------------------------------------`n" -ForegroundColor $color
+}
 
-dotnet test ./src/ChickenScratch.sln -c Release
+$currBranch = git branch --show-current
 
-if (-Not $?) {
-    Write-Output "!!!!Tests failed"
+if ($currBranch -ne "master") {
+    Write-Status "Not on master branch" Failure
     exit 1
 }
 
+Write-Host "`n`nEnter the Nuget API Key: " -ForegroundColor Magenta -NoNewline
+$apiKey = Read-Host
+
+Remove-item ./publish -Recurse 
+
+Write-Status "Executing Tests" 
+dotnet test ./src/ChickenScratch.sln -c Release
+
+if (-Not $?) {
+    Write-Status "Tests Failed" Failure
+    exit 1
+}
+
+Write-Status "Creating Packages" 
 dotnet pack ./src/ChickenScratch.sln -c Release -o ./publish
 
 $pkgs = Get-Item ./publish/*.nupkg 
 
 foreach ($pkg in $pkgs) {
+    Write-Status ("Publishing {0}" -f $pkg) 
     dotnet nuget push $pkg.FullName -s https://api.nuget.org/v3/index.json -k $apiKey
+
+    if (-Not $?) {
+        Write-Status "Publishing Failed" Failure
+        exit 1
+    }    
 }
+
+Write-Status "Packages Published" Success
